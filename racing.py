40cb5d35f4d2de94c1c0a80b27ac2e7d5a67669e
Andres Goens <goens@goenshome>
2017-01-04 22:38:31 +0100

    Added camera following the car and larger levels. Missing: boundary conditions

--- racing.py
+++ racing.py
@@ -26,6 +26,10 @@ VIDEO_FLAGS = 0
 FRAMES_PER_SECOND = 30
 CAMERA_SLACK = 30
 
+MAX_WORLD_SIZE_MULTIPLIER = 5
+
+TITLE_STRING = " Drakoon Racing!"
+
 #other variables
 RACINGGAME_ROOT = os.path.realpath(argv[0]).split('/')
 RACINGGAME_ROOT.pop(-1)
@@ -41,18 +45,6 @@ TREE = FIGS + "tree.png"
 TREE_EXPLOSION = FIGS + "tree_explosion.png"
 
 BACKGROUND = FIGS + 'background.png'
-#--------------------------------
-# setup
-#--------------------------------
-clock = pygame.time.Clock()
-deltat = clock.tick(FRAMES_PER_SECOND)
-
-#for testing:
-screen = pygame.display.set_mode(DISPLAY, VIDEO_FLAGS, DEPTH)
-
-#for gameplay
-#screen = pygame.display.set_mode((1024,768),FULLSCREEN, DOUBLEBUFF)
-#pygame.display.flip()
 
 #--------------------------------
 # sprite classes
@@ -71,6 +63,7 @@ class KorandoSprite(pygame.sprite.Sprite):
         self.src_images = []
         for img in [KORANDO, KORANDO_SMOKE, KORANDO_FIRE, KORANDO_EXPLOSION]: 
             self.src_images.append(pygame.image.load(img))
+        self.image = self.src_images[0]
         self.position = position
         self.speed = 0 
         self.acceleration = 0 
@@ -119,6 +112,7 @@ class TreeSprite(pygame.sprite.Sprite):
     def __init__(self, position):
         self.rect = pygame.Rect(self.normal.get_rect())
         self.rect.center = position
+        self.image = self.normal
         pygame.sprite.Sprite.__init__(self)
     def update(self, hit_list):
         #print(str(hit_list))
@@ -128,72 +122,115 @@ class TreeSprite(pygame.sprite.Sprite):
            self.image = self.hit
         else: self.image = self.normal
 
+#--------------------------------
+# other game classes
+#--------------------------------
 class GameInstance:
-    def __init__(self):
-        self.korando = KorandoSprite(FIGS + 'korando.png', rect.center)
+    def __init__(self,screen):
+        self.korando = KorandoSprite(FIGS + 'korando.png', screen.get_rect().center)
         self.korando_group = pygame.sprite.RenderPlain(self.korando)
         
-        num_trees = random.randint(3,11)
-        self.trees = []
-        for i in range(num_trees):
-            x,y = random.randint(0,1024), random.randint(0,768)
-            self.trees.append(TreeSprite((x,y)))
+        self.level_height = random.randint(WIN_HEIGHT,MAX_WORLD_SIZE_MULTIPLIER * WIN_HEIGHT)
+        self.level_width = random.randint(WIN_WIDTH,MAX_WORLD_SIZE_MULTIPLIER * WIN_WIDTH)
+        self.num_trees = random.randint(3,11) * int(self.level_width/WIN_WIDTH)* int(self.level_height/WIN_HEIGHT)
+        self.entities = pygame.sprite.Group()
+        self.tree_group = pygame.sprite.Group()
+        self.entities.add(self.korando)
+        for i in range(self.num_trees):
+            x,y = random.randint(0,self.level_height), random.randint(0,self.level_width)
+            tree = TreeSprite((x,y))
+            self.entities.add(tree)
+            self.tree_group.add(tree)
         
-        self.tree_group = pygame.sprite.RenderPlain(*self.trees)
-        pygame.display.flip()
+        #self.tree_group = pygame.sprite.RenderPlain(*self.trees)
          
     
-    def clear_game(self):
-        try:
-           self.korando.kill()
-        except:
-           self.korando = None
-        try:
-            for tree in self.trees:
-                tree.kill()
-            self.trees = []
-        except:
-            self.trees = []
+    def clear_game(self,screen,background):
+        for e in self.entities:
+            e.kill()
         screen.blit(background, (0,0))
+
+#from: http://stackoverflow.com/questions/14354171/add-scrolling-to-a-platformer-in-pygame 
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = pygame.Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+def simple_camera(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    return Rect(-l+HALF_WIDTH, -t+HALF_HEIGHT, w, h)
+
+def complex_camera(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t, _, _ = -l+HALF_WIDTH, -t+HALF_HEIGHT, w, h
+
+    l = min(0, l)                           # stop scrolling at the left edge
+    l = max(-(camera.width-WIN_WIDTH), l)   # stop scrolling at the right edge
+    t = max(-(camera.height-WIN_HEIGHT), t) # stop scrolling at the bottom
+    t = min(0, t)                           # stop scrolling at the top
+    return pygame.Rect(l, t, w, h)
+
+#--------------------------------
+# main loop
+#--------------------------------
+def main():
+    clock = pygame.time.Clock()
+    deltat = clock.tick(FRAMES_PER_SECOND)
     
-#--------------------------------
-# game loop
-#--------------------------------
-background = pygame.image.load(BACKGROUND)
-screen.blit(background, (0,0))
-rect = screen.get_rect()
+    #for testing:
+    screen = pygame.display.set_mode(DISPLAY, VIDEO_FLAGS, DEPTH)
+        
+    pygame.display.set_caption(TITLE_STRING)
+    background = pygame.image.load(BACKGROUND)
+    screen.blit(background, (0,0))
+    rect = screen.get_rect()
+    
+    game = GameInstance(screen)
+    restart = False
+    camera = Camera(complex_camera, game.level_width, game.level_height)
+    
+    while 1:
+        # user input
+        deltat = clock.tick(30)
+        for event in pygame.event.get():
+            if not hasattr(event, 'key'): continue
+            down = event.type == pygame.KEYDOWN
+            if event.key == pygame.K_RIGHT: game.korando.k_right = down * 5
+            elif event.key == pygame.K_LEFT: game.korando.k_left = down * 5
+            elif event.key == pygame.K_UP: game.korando.k_up = down * 2
+            elif event.key == pygame.K_DOWN: game.korando.k_down = down * 2
+            elif event.key == pygame.K_DELETE: exit(0)
+            elif event.key == pygame.K_r and event.type == pygame.KEYUP: restart = True
+    
+        # rendering
+        screen.blit(background, (0,0))
+        camera.update(game.korando)
+        for e in game.entities:
+            screen.blit(e.image, camera.apply(e))
+    
+        if restart == True:
+            game.clear_game(screen,background)
+            del game
+            game = GameInstance(screen)
+            del camera
+            camera = Camera(complex_camera, game.level_width, game.level_height)
+            restart = False
+    
+        collisions = pygame.sprite.groupcollide(game.tree_group, game.korando_group, 0, 0)
+        #print("collisions: " + str(collisions))
+        game.tree_group.update(collisions)
+        game.korando_group.update(deltat,collisions)
+        game.korando_group.update(deltat,collisions)
+    
+        pygame.display.flip()
 
-game = GameInstance()
-restart = False
-
-while 1:
-    # USER INPUT
-    deltat = clock.tick(30)
-    for event in pygame.event.get():
-        if not hasattr(event, 'key'): continue
-        down = event.type == pygame.KEYDOWN
-        if event.key == pygame.K_RIGHT: game.korando.k_right = down * 5
-        elif event.key == pygame.K_LEFT: game.korando.k_left = down * 5
-        elif event.key == pygame.K_UP: game.korando.k_up = down * 2
-        elif event.key == pygame.K_DOWN: game.korando.k_down = down * 2
-        elif event.key == pygame.K_DELETE: exit(0)
-        elif event.key == pygame.K_r and event.type == pygame.KEYUP: restart = True
-    # RENDERING
-    game.tree_group.clear(screen, background)
-    game.korando_group.clear(screen, background)
-
-    if restart == True:
-        game.clear_game()
-        del game
-        game = GameInstance()
-        restart = False
-
-    collisions = pygame.sprite.groupcollide(game.tree_group, game.korando_group, 0, 0)
-    #print("collisions: " + str(collisions))
-    game.tree_group.update(collisions)
-    game.korando_group.update(deltat,collisions)
-    game.korando_group.update(deltat,collisions)
-
-    game.tree_group.draw(screen)
-    game.korando_group.draw(screen)
-    pygame.display.flip()
+if __name__ == "__main__":
+    main()
